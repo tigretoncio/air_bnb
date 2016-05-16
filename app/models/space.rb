9c499b66e8e7a6ea0d655b0b169c033dce7176bd
Amy Nicholson <missamynicholson@gmail.com>
2016-05-16 15:53:59 +0100

    Model map
    Decided on relationships - three objects(User, Space, Booking).
    All have One-to_Many relationships.
    Used belongs_to for the relationships (may need to add has n if this doesn't work)

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,7 +5,6 @@ class Space
   property :name, Text, required: true
   property :description, Text, required: true
   property :price, Integer, required: true
-  property :created_at, DateTime, required: true
 
   belongs_to :user, required: true
 
