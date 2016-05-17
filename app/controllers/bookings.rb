7da5b2eef8433d9fdbed972592cc3a79c28af3d8
MariaRomero <fantasysystems@gmail.com>
2016-05-17 16:24:20 +0100

    view of bookings page with list of bookings

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -1,14 +1,19 @@
 class AirBnb < Sinatra::Base
 
+  get "/bookings" do
+    @bookings = Booking.all
+    erb :"bookings/index"
+  end
+
   post "/bookings/new" do
     booking = Booking.create(user: current_user,
                              space: Space.first(id: params[:space]),
-                             date: params[:date])
-    # if booking.id.nil?
-    #   flash[:errors] = space.errors.full_messages
-    #   redirect to "/spaces/new"
-    # else
-    #   redirect to "/spaces"
-    # end
+                             date: params[:date],
+                             status: "Requested")
+    if booking.id.nil?
+      flash[:errors] = booking.errors.full_messages
+    else
+      redirect to "/bookings"
+    end
   end
 end
--- spec/features/view_bookings_spec.rb
+++ spec/features/view_bookings_spec.rb
@@ -1,26 +1,21 @@
 feature "view booking" do
 
-  before do
-    sign_up
-    create_space
+  scenario "view list of bookings" do
+    create_booking
+    click_button "Request to book"
+    expect(page).to have_content "My Bookings"
   end
 
   scenario "view list of bookings" do
     visit "/bookings"
-    expect(page).to have_content "Amy's house"
+    expect(page).to have_content "My Bookings"
   end
-
-  scenario "view list of bookings" do
-    visit "/spaces"
-    expect(page).to have_content "Amy's house"
-    expect(page).to have_content "Amy's other house"
-  end
-
-  scenario "view list of bookings in ascending date order" do
-    params1= {date: Time.new(2016,5,25), status: "Requested", user: User.first}
-    create_space(params1)
-    first_space_index = page.body.index("Amy's house")
-    second_space_index = page.body.index("Amy's other house")
-    expect(first_space_index).to be > second_space_index
-  end
+  #
+  # scenario "view list of bookings in ascending date order" do
+  #   params1= {date: Time.new(2016,5,25), status: "Requested", user: User.first}
+  #   create_space(params1)
+  #   first_space_index = page.body.index("Amy's house")
+  #   second_space_index = page.body.index("Amy's other house")
+  #   expect(first_space_index).to be > second_space_index
+  # end
 end
