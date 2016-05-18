7a6951f8eda48ee49faa8e991ad964133248b9bb
MariaRomero <fantasysystems@gmail.com>
2016-05-18 16:17:01 +0100

    fixed denying all bookings

--- app/controllers/bookings.rb
+++ app/controllers/bookings.rb
@@ -34,9 +34,11 @@ class AirBnb < Sinatra::Base
   post "/bookings/:id/:status" do
     booking = Booking.first(id: params[:id])
     booking.update(status: params[:status])
-    other_bookings = Booking.all(space_id: booking.space.id,
-                                  date: booking.date) - booking
-    other_bookings.update(status: "denied")
+    if  params[:status] == "confirmed"
+      other_bookings = Booking.all(space_id: booking.space.id,
+                                    date: booking.date) - booking
+      other_bookings.update(status: "denied")
+    end
     redirect to "/bookings"
   end
 end
