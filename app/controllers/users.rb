1759b797b625353fa86c8fdf91aa1ac8fcc8f03d
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-19 15:21:52 +0100

    send email for signup works, but without ENV variables

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,4 @@
+require 'mailgun'
 class AirBnb < Sinatra::Base
 
   get "/users/new" do
--- spec/send_signup_ack_spec.rb
+++ spec/send_signup_ack_spec.rb
@@ -1,10 +1,12 @@
 require './app/lib/send_signup_ack'
 
 describe SendSignupAck do
+  DOMAIN = "sandbox095db3c079904d219e00440da5443f4c.mailgun.org"
  let(:user){double :user, email: "test@test.com", password_token: "12345678"}
  let(:mail_gun_client){double :mail_gun_client}
- let(:sandbox_domain_name) { ENV["sandbox_domain_name"] }
+ let(:sandbox_domain_name) { DOMAIN }
 
+# ENV["sandbox_domain_name"]
  it "sends a message to mailgun when it is called" do
    params = {from: "airbnb@mail.com",
              to: user.email,
